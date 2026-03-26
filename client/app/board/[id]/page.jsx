1a3a3527474ec3f82f89a2f024ff95781c4116e8
snehal prakash <smhsneh@gmail.com>
2026-03-26 23:59:05 +0530

    u11 ; stickers panel

--- client/app/board/[id]/page.jsx
+++ client/app/board/[id]/page.jsx
@@ -1,11 +1,13 @@
 "use client";
-
+import { useState } from "react";
 import Canvas from "../../../components/canvas/Canvas";
 import { useBoardStore } from "../../../store/board-store/board-store";
 
 export default function BoardPage() {
   const addNote = useBoardStore((state) => state.addNote);
   const addSticker = useBoardStore((state) => state.addSticker);
+
+  const [showStickers, setShowStickers] = useState(false);
 
   return (
     <div
@@ -72,13 +74,64 @@ export default function BoardPage() {
             boxShadow: "0 10px 25px rgba(0,0,0,0.1)",
           }}
         >
+          {showStickers && (
+            <div
+              style={{
+                position: "absolute",
+                left: "90px",
+                top: "50%",
+                transform: "translateY(-50%)",
+                background: "white",
+                padding: "12px",
+                borderRadius: "16px",
+                boxShadow: "0 10px 30px rgba(0,0,0,0.1)",
+                display: "grid",
+                gridTemplateColumns: "repeat(3, 60px)",
+                gap: "10px",
+                zIndex: 20,
+              }}
+            >
+              {[
+                "envelope",
+                "flower",
+                "glitter",
+                "heart",
+                "panic",
+                "quote",
+                "slay",
+                "smiley",
+                "star",
+                "study",
+                "teddy",
+                "warning",
+                "work",
+              ].map((name) => (
+                <img
+                  key={name}
+                  src={`/stickers/${name}.png`}
+                  onClick={() => {
+                    addSticker(`/stickers/${name}.png`);
+                    setShowStickers(false);
+                  }}
+                  style={{
+                    width: "50px",
+                    height: "50px",
+                    objectFit: "contain",
+                    cursor: "pointer",
+                    borderRadius: "10px",
+                    padding: "6px",
+                  }}
+                />
+              ))}
+            </div>
+          )}
           <button onClick={addNote} className="tool-btn">
             note
           </button>
           <button className="tool-btn">color</button>
           <button
             className="tool-btn"
-            onClick={() => addSticker("/stickers/star.png")}
+            onClick={() => setShowStickers((prev) => !prev)}
           >
             stickers
           </button>
